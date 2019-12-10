a549b71da27d9c43f11e4eaa97917e1a560e1117
meny azulay <menyazol@outlook.com>
2019-12-10 20:43:24 +0200

    added a new footer [newBranch]

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a sentense:");
+            string a = Console.ReadLine();
+            Console.WriteLine("strat read from:");
+            int start = Convert.ToInt16(Console.Read());
+            Console.WriteLine("number of chars to read");
+            int count = Convert.ToInt16(Console.Read());
+            List<char> result = new List<char>();
+
+            for (int i = start; i < (start + count); i++)
+                result.Add(a[i]);
+
+            foreach (char c in result)
+                Console.WriteLine("{0}" , c);
+
+            Console.Read();
+        }
+    }
+}
